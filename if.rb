71809638e2051f09a57728522ff57c72d0369078
s1240174 <s1240174@u-aizu.ac.jp>
2016-05-12 19:00:07 +0900

    p

---
+++ if.rb
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*- 
+
+if 1<2 then
+  puts "aaaaa"
+end
+
+if false then
+  puts "false is false"
+end
+
+if 1<2 then
+  puts "aaaaaaa"
+end
+
+if 1==2 then
+  puts"bbbbbbbbbb"
+end
---
+++ test.rb
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*
+
+puts "入力してください:"
+x = gets
+puts "x=#{x}"
